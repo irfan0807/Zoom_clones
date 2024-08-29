6b7f1c62c7864ea0740290f924e87e6f31bd8ea6
Shaik Irfan <irfandbs507@gmail.com>
2024-08-29 14:24:44 +0530

    CallLayout and feature to show and hide partispant and Speaker-locatin on screen added

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import { CallControls, CallParticipantsList, PaginatedGridLayout, SpeakerLayout } from "@stream-io/video-react-sdk";
+import React, { useState } from "react";
+import { cn } from "@/lib/utils";
+
+type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
 const MeetingRoom = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [layout, setLayout] = useState("speaker-left");
+  const [show ,setShow] = useState(false);
 
-export default MeetingRoom
+  const CallLayout = () => {
+    switch (layout) {
+      case "grid":
+        return <PaginatedGridLayout />;
+      case "speaker-left":
+        return <SpeakerLayout participantsBarPosition={"right"} />;
+      default:
+        return <SpeakerLayout participantsBarPosition={"left"} />;
+    }
+  };
+
+  return (
+    <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
+      <div className="relative flex size-full items-center justify-center">
+        <div className="flex size-full max-w-[1000px] items-center">
+        <CallLayout />
+        </div>
+        <div className={cn("h-[calc(100vh-86px)] hidden ml-2", {'show-block':show} )}>
+          <CallParticipantsList  onClose={() => {
+            setShow(false)
+          }}/>
+        </div>
+      </div>
+      <div className="fixed buttom-0 flex w-full items-center justify-center gap-5">
+        <CallControls />
+
+      </div>
+    </section>
+  );
+};
+
+export default MeetingRoom;
--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,8 +1,9 @@
 'use client'
-import { useCall, VideoPreview } from "@stream-io/video-react-sdk";
+import { DeviceSettings, useCall, VideoPreview } from "@stream-io/video-react-sdk";
 import React, { useEffect, useState } from "react";
+import { Button } from "./ui/button";
 
-const MeetingSetup = () => {
+const MeetingSetup = ({setIsSetupComplete} : {setIsSetupComplete : (val:boolean) => void}) => {
   const [isMicCameraToggleOn, setisMicCameraToggleOn] = useState(false);
   const call = useCall();
 
@@ -26,6 +27,20 @@ const MeetingSetup = () => {
     <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
       <h1 className="text-2xl font-bold">Setup</h1>
       <VideoPreview />
+      <div className="flex h-16 items-center justify-center gap-3">
+        <label className="flex items-center justify-center gap-2 font-medium">
+            <input type='checkbox' checked={isMicCameraToggleOn} onChange={(e) => {setisMicCameraToggleOn(e.target.checked)}}/>
+            Join with mic and Camera off
+        </label>
+        <DeviceSettings />
+
+      </div>
+      <Button className="rounded-md bg-green-500 px-4 py-2.5" onClick={() => {
+        call.join()
+        setIsSetupComplete(true);
+      }}>
+        Join Meeting
+      </Button>
     </div>
   );
 };
